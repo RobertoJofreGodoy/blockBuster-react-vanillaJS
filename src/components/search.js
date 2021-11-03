fc8511fb7e6401c1a0ae6d5d186a01cd2984a58c
RobertoJofreGodoy <roberto.jofre.godoy.dev@gmail.com>
2021-11-03 19:09:35 -0300

    Acción de Búsqueda

--- src/components/search.js
+++ src/components/search.js
@@ -2,18 +2,37 @@ import { Component, createElement } from "../lib/react/index.js"
 import Form from "./form.js"
 import Input from "./input.js"
 import button from "./button.js"
+import { store } from "../redux/store/index.js"
+import { SEARCH_MOVIE, SET_FILTER } from "../redux/actions/index.js"
 
 class Search extends Component {
-  render() {
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const query = formData.get("title")
 
+    if (query) {
+      return store.dispatch({
+        type: SEARCH_MOVIE,
+        payload: query,
+      })
+    }
+    return store.dispatch({
+      type: SET_FILTER,
+      payload: "all",
+    })
+  }
+
+  render() {
     return Form({
+      onSubmit: this.handleSubmit,
       children: [
         new Input({
           placeholder: "Escribe tu película favorita",
           name: "title",
-          type: 'text'
+          type: "text",
         }),
-        new button(null, 'Buscar')
+        new button(null, "Buscar"),
       ],
     })
   }
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,30 @@
-import { ADD_MOVIES, SET_FILTER } from "../actions/index.js"
+import { ADD_MOVIES, SET_FILTER, SEARCH_MOVIE } from "../actions/index.js"
 import { movieListMap, getAllIds, getMostValuedIds, getLeastValuedIds } from '../../normalize.js'
+
+function filterByTitle(title, movies) {
+  const list = []
+  movies.forEach((movie) => {
+    if (movie.title.toLowerCase().includes(title.toLowerCase())) {
+      list.push(movie.id)
+    }
+  })
+  return list
+}
+
+function filterById(id, allIds) {
+  const parseId = parseInt(id, 10)
+  if (allIds.includes(parseId)) {
+    return [parseId]
+  }
+  return allIds
+}
+
+function searchMovie(query, list, allIds) {
+  if (isNaN(parseInt(query))) {
+    return filterByTitle(query, list)
+  }
+  return filterById(query, allIds)
+}
 
 export default function reducer(state, { type, payload }) {
   switch (type) {
@@ -24,6 +49,15 @@ export default function reducer(state, { type, payload }) {
       return { 
         ...state,
         filter: payload
+      }
+    case SEARCH_MOVIE:
+      return { 
+        ...state,
+        filter: 'search',
+        list: {
+          ...state.list,
+          search: searchMovie(payload, state.movieList, state.list.all)
+        }
       }
     default:
       return state
